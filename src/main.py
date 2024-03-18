d21f00126d942172df4bf39bd1610841aa56ca03
Arnstein Kleven <arnsteinkleven@gmail.com>
2024-03-18 15:58:26 +0100

    wip

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import sys
+import os
 from PyQt6.QtWidgets import QMainWindow, QApplication, QTreeView, QWidget, QVBoxLayout
 from PyQt6.QtGui import QIcon, QFileSystemModel
 from PyQt6.QtCore import QDir, QStringListModel
 from MainWindow import Ui_MainWindow
 # TODO:
-# Add / remove items from list
-#   if folder: add all subitems
+# Show items to convert in bottom list
 # Set ffmpeg command in Options
+# Make exit button work
 # Convert
+# Make sure it works on Windows / convert
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
@@ -18,6 +20,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.destinationModel = QFileSystemModel()
         self.destinationModel.setRootPath(QDir.rootPath())
         self.files_to_convert = []
+        self.list_focus = ""
+        self.list_focus_2 = ""
 
         self.path = QDir.rootPath()
 
@@ -31,27 +35,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.treeView_2.setRootIndex(self.destinationModel.index(self.path))
 
         self.treeView.clicked.connect(self.on_clicked)
+        self.treeView_2.clicked.connect(self.on_clicked_destination)
         self.listView.clicked.connect(self.on_clicked_list)
         self.listView_2.clicked.connect(self.on_clicked_list_2)
 
         self.listView.setModel(self.fileModel)
         self.listView_2.setModel(self.listModel)
 
-        self.pushButton_2.clicked.connect(self.add_file)
         self.pushButton.clicked.connect(self.convert)
+        self.pushButton_2.clicked.connect(self.add_file)
+        self.pushButton_3.clicked.connect(self.remove_file)
 
 
     def add_file(self):
-        self.files_to_convert.append(self.list_focus)
+        if os.path.isdir(self.list_focus):
+            for item in os.listdir(self.list_focus):
+                print(item)
+                if ".wav" in item:
+                    print("is a wav file, adding")
+                    self.files_to_convert.append(self.list_focus + f"/{item}")
+        else:
+            if ".wav" in self.list_focus:
+                self.files_to_convert.append(self.list_focus)
         print(self.files_to_convert)
 
     def remove_file(self):
         self.files_to_convert.remove(self.list_2_focus)
         print(self.files_to_convert)
 
+    def on_clicked_destination(self, index):
+        self.destination = self.destinationModel.fileInfo(index).absoluteFilePath()
+
     def on_clicked(self, index):
         self.path = self.directoryModel.fileInfo(index).absoluteFilePath()
         self.listView.setRootIndex(self.fileModel.setRootPath(self.path))
+        if self.list_focus == "":
+            self.list_focus = self.path
 
     def on_clicked_list(self, index):
         self.list_focus = self.directoryModel.fileInfo(index).absoluteFilePath()
@@ -60,10 +79,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.list_2_focus = self.directoryModel.fileInfo(index).absoluteFilePath()
 
     def convert(self):
-        print("Converting!")
         for item in self.files_to_convert:
+            print(f"Converting {item}!")
             # self.ffmpeg_command
-            self.files_to_convert(item)
+            self.files_to_convert.remove(item)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
